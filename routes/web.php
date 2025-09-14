96a5a4bf3abfd9be989c4c2a758fe87a94b053e3
Fahril Hadi <fahrilhadi77@gmail.com>
2025-09-14 18:48:25 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('pages.dashboard', ['type_menu' => 'dashboard']);
+    })->name('dashboard');
 });
